c37b2d3dea03ea350b6bdbc21fcda113972f9d0e
Sergey Savchuk <savchuk.sergey@gmail.com>
2015-09-23 16:30:48 +0300

    vs2015

--- ChessRun.Engine/Program.cs
+++ ChessRun.Engine/Program.cs
@@ -3,7 +3,7 @@
 namespace ChessRun.Engine {
     class Program {
 
-        private static readonly ChessEngineApi _engine = new ChessEngineApiM();
+        private static readonly ChessEngineApi _engine = new ChessEngineApi();
 
         static void Main(string[] args) {
             while (true) {
